f3af34d295df299f65fef32db72f25d155fd9cad
itisaby <arnabbibhuti01@gmail.com>
2022-01-15 12:14:11 +0530

    Find Pivot with duplicate elements

--- src/RotatedBinarySearch.java
+++ src/RotatedBinarySearch.java
@@ -1,8 +1,8 @@
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int[] arr = {3,5,1};
-        System.out.println(findPivot(arr));
-        System.out.println(search(arr, 3));
+        int[] arr = {2, 2, 9, 2, 2, 3};
+        System.out.println(findPivotWithDuplicate(arr));
+        System.out.println(search(arr, 2));
     }
 //this will not cover duplicate values
     
@@ -26,6 +26,44 @@ public class RotatedBinarySearch {
         }
         return -1;
     }
+    static public int findPivotWithDuplicate(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+//            case1
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid]<arr[mid-1]){
+                return  mid-1;
+            }
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     static int Binarysearch(int[] arr, int target, int start, int end) {
         while(start <= end) {
             int mid = start + (end - start) / 2;
