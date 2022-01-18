62c5b1b95f96ca0a5e3b36e18e20e96f079f5e3e
itisaby <arnabbibhuti01@gmail.com>
2022-01-18 12:14:08 +0530

    Perfect Square problem added

--- src/ValidPerfectSquare.java
+++ src/ValidPerfectSquare.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ValidPerfectSquare {
+public class ValidPerfectSquare {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isPerfectSquare(int num) {
+        int start=2;
+        int mid,end=num;
+        if (num<2){
+            return true;
+        }
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if (mid==((float)num/mid)){
+                return true;
+            }
+            else if (mid<(num/mid)){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return false;
+    }
 }
