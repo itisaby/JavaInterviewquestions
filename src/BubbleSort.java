dc19ba86a32c58730b7b72735d2911ac2e3646af
itisaby <arnabbubhuti01@gmail.com>
2022-02-20 13:16:06 +0530

    Bubble Sort added

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 1, 2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        boolean swapped;
+        for(int i=0; i<arr.length; i++){
+            swapped = false;
+            for(int j=1;j< arr.length-i; j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            //if it is not swapping for a particular value of i it means it's already sorted so it's better to stop it at that moment
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
