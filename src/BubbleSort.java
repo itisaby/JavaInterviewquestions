0716a34bb7a77aebec2d2aff98fa909ce5ca0c3f
itisaby <arnabbibhuti01@gmail.com>
2022-02-20 13:19:07 +0530

    Bubble Sort added

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {3, 4, 5, 1, 2};
+        int[] arr = {6, 7, 3, 11, 8, 9};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
