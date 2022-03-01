dfafe3f242d92d75a3f7614317e561c011fd8938
itisaby <arnabbibhuti01@gmail.com>
2022-03-01 12:28:56 +0530

    Added palindrome prob

---
+++ src/Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String s = "abcba";
+        System.out.println(isPalindrom(s));
+    }
+    static boolean isPalindrom(String s){
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length()/2; i++) {
+            char start = s.charAt(i);
+            char end = s.charAt(s.length()-1-i);
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
