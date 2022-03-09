f4188f3d82f2f11cbe35ea5ae67ea7919cf2ba18
itisaby <arnabbibhuti01@gmail.com>
2022-03-09 16:13:00 +0530

    BS using recursion

--- src/MultiRec.java
+++ src/MultiRec.java
@@ -1,11 +1,27 @@
 public class MultiRec {
     public static void main(String[] args) {
-        System.out.println(Fibo(6));
+//        System.out.println(Fibo(6));
+        int[] arr = new int[]{1, 2, 44, 121, 44, 65, 3};
+        int target = 44;
+        System.out.println(BinarySearchwithR(arr, arr[0], arr[arr.length-1], target));
     }
     public static int Fibo(int n){
         if(n<=1){
             return n;
         }
         return Fibo(n-1) + Fibo(n-2);
+    }
+    public static int BinarySearchwithR(int[] arr, int start, int end, int target){
+        int mid = start + (end-start)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(arr[mid]>target){
+            BinarySearchwithR(arr, mid+1, end, target);
+        }
+        if(arr[mid]<target){
+            BinarySearchwithR(arr, start, mid-1, target);
+        }
+        return -1;
     }
 }
