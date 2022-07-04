9a727367b1d1900b610b5dc2884de7e232edcc9c
itisaby <arnabbibhuti01@gmail.com>
2022-07-04 17:18:39 +0530

    String methods added

--- src/StringsBasics.java
+++ src/StringsBasics.java
@@ -1,7 +1,21 @@
 public class StringsBasics {
     public static void main(String[] args) {
-        String name = "Arnab Maity";
+        String name = "Arnab Maity";  //string is immutable
+        String name1 = "Arnab Maity";
         System.out.println(name);
+//        Creating two new object
+        String s = new String("Arnab");
+        String s2 = new String("Arnab");
+        System.out.println(s==s2);
+        System.out.println(s.equals(s2));
+        System.out.println(name==name1);
+        String a = name;
+        System.out.println(a);
+        System.out.println(a.charAt(1));
+        //Comparison Of strings
+        // == method -> Comparator -> check for values and the reference variables pointing to same object
+        // .equals() -> Checks for the value
+
 
     }
 }
