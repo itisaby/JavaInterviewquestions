8f4e0fb2a3c26989295b690f13a7087bbb454f30
itisaby <arnabbibhuti01@gmail.com>
2022-07-17 23:41:29 +0530

    MAin.java added

---
+++ src/aby/introduction/Main.java
@@ -0,0 +1,11 @@
+package aby.introduction;
+
+import static aby.packages.BasicPack.message;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Printing Main");
+        message();
+        new Greetings();
+    }
+}
