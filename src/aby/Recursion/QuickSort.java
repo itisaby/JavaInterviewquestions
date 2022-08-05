cd748579e36727e9b32947273f4b063f3c0e0c86
itisaby <arnabbibhuti01@gmail.com>
2022-08-05 11:04:51 +0530

    Quick Sort added

---
+++ src/aby/Recursion/QuickSort.java
@@ -0,0 +1,38 @@
+package aby.Recursion;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        QuickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void QuickSort(int[] nums, int l, int h){
+        if(l>=h){
+            return;
+        }
+        int s = l;
+        int e = h;
+        int mid = s + (e-s)/2;
+        int pivot = nums[mid];
+        while(s<=e){
+            while(nums[s]<pivot){
+                s++;
+            }
+            while(nums[e] > pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp = nums[s];
+                nums[s] =nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        //pivot at correct index
+        QuickSort(nums, l, e);
+        QuickSort(nums, s, h);
+    }
+}
